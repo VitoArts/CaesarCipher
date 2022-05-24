2af6351f0398c9824094fb7a1111d1de0d9a1761
Vito <vito.arts@hotmail.com>
2022-05-24 23:16:25 +0200

    Added spaces between ciphered words

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+
+alphabetDict = {}
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+position = 0
+spaceKeeper = []
+
 def ceasarCipher(input, key):
     for x in range(len(input)):
         for index in alphabetDict:
@@ -5,16 +11,21 @@ def ceasarCipher(input, key):
                 position = index
         newChar = alphabetDict[(position + int(key)) % 26]
         input = input[:x] + newChar + input[x+1:]
+    if spaceKeeper != []:
+        for space in range(len(spaceKeeper)):
+            input = input[:spaceKeeper[space]] + ' ' + input[spaceKeeper[space]+1:]
     print(input)
 
+def findSpace(input):
+    for index in range(len(input)):
+        if input[index] == ' ':
+            spaceKeeper.append(index)
 
 if __name__ == '__main__':
-    alphabetDict = {}
-    alphabet = "abcdefghijklmnopqrstuvwxyz"
-    position = 0
     for x in range(26):
         alphabetDict[x+1] = alphabet[x]
-
     inputStr = input("Enter your string: ")
+    if inputStr.count('') > 0:
+        findSpace(inputStr)
     inputKey = input("Enter your key (int): ")
     ceasarCipher(inputStr, inputKey)
